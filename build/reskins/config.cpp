be96107ad5d28854626dd7ca8a78d8f327818c3b
Karan Singh <karan.vir.singh.kvs@gmail.com>
2020-03-30 18:15:37 -0400

    Medical Chimera (Green)

--- build/reskins/config.cpp
+++ build/reskins/config.cpp
@@ -6,34 +6,39 @@ class CfgPatches
 	{
 		author = "Konigstiger";
 		name = "DK143 TIOW/Grimdark Reskins";
-		requiredAddons[] = 
-        {"ace_common","CadFoot"};
+		requiredAddons[] = {"ace_common"};
 		requiredVersion = 0.1;
-		units[] = {};
+		units[] = {"143rd_Chimera_Green_Medical"};
 		weapons[] = {};
 		worlds[] = {};
 	};
 };
 
-//TODO
-//Replace chimera_base with actual GD chimera baseclass
-//Replace faction with 143rd Custom Faction
-//Add custom crew
-//Point to textures
+class CfgFactionClasses
+{
+	class B_143rd_DKoK
+	{
+		displayName = "143rd DKoK";
+		flag = "\reskins\data\faction.paa";
+		icon = "\reskins\data\faction.paa";
+		priority = 1;
+		side = 1;
+	};
+};
 
 class cfgVehicles {
 
-    class chimera_base;
-    class 143rd_Chimera_Green_Medical: chimera_base
+    class ML700_Chimera_Autocannon;
+    class 143rd_Chimera_Green_Medical: ML700_Chimera_Autocannon
     {
-        _generalMacro = "chimera_base";
+        _generalMacro = "ML700_Chimera_Autocannon";
         scope = 2;
         side = 1;
-        faction = "REPLACE ME";
-        displayName = "143rd Medical Chimera (Green)"
-        crew = "crewclassname, crewclassname"
+        faction = "B_143rd_DKoK";
+        displayName = "143rd Medical Chimera (Green)";
+        crew = "ML700_Cadian_Rifleman";
+        typicalCargo[] = {"ML700_Cadian_Rifleman"};
+        hiddenSelections[] = {"camo1","camo2","camo3","camo4","camo5","camo6","camo7","camo8","camo9"};
+        hiddenSelectionsTextures[] = {"reskins\data\land_vehicles\gd_chimera\medical_green\driveassembly_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\hull_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\hullheavybolter_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\hullmisc_ca.paa","reskins\data\land_vehicles\gd_chimera\medical_green\interior_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\lasarray_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\reardoor_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\turretexterior_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\turretweapons_co.paa"};
     };
-
 };
-
-
