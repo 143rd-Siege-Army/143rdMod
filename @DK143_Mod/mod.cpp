ef4f34ee9f784135c955738a9b6ae68249560af8
Eric <esigde13@gmail.com>
2020-04-14 17:13:12 -0500

    mod PBO release

---
+++ @DK143_Mod/mod.cpp
@@ -0,0 +1,12 @@
+name = "DK143 Compatability Mod";
+picture = "DK143_logo.paa";
+actionName = "GitHub";
+action = "https://github.com/143rd-Siege-Army/143rdMod";
+description = "DK143 - Version Beta 1.0";
+logo = "DK143_logo.paa";
+logoOver = "DK143_logo.paa";
+tooltip = "DK143";
+tooltipOwned = "DK143 Owned";
+overview = "The DK143 Compatability Mod aims to act as a compatability and balance mod between the ACE3 environment and the TIOW 40K mod.";
+author = "DK143 Mod Staff";
+overviewPicture = "DK143_logo.paa";
