9140daba72780499b9c501e24aed4100fd58912e
Karan Singh <karan.vir.singh.kvs@gmail.com>
2020-04-15 00:51:31 -0400

    143rd TIOW Tauroxes

--- build/reskins/config.cpp
+++ build/reskins/config.cpp
@@ -41,4 +41,30 @@ class cfgVehicles {
         hiddenSelections[] = {"camo1","camo2","camo3","camo4","camo5","camo6","camo7","camo8","camo9"};
         hiddenSelectionsTextures[] = {"reskins\data\land_vehicles\gd_chimera\medical_green\driveassembly_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\hull_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\hullheavybolter_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\hullmisc_ca.paa","reskins\data\land_vehicles\gd_chimera\medical_green\interior_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\lasarray_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\reardoor_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\turretexterior_co.paa","reskins\data\land_vehicles\gd_chimera\medical_green\turretweapons_co.paa"};
     };
+	class TIOW_Taurox_HS_667;
+    class 143rd_Taurox_Grey_Base_HS: TIOW_Taurox_HS_667
+    {
+        _generalMacro = "TIOW_Taurox_HS_667";
+        scope = 2;
+        side = 1;
+        faction = "B_143rd_DKoK";
+        displayName = "143rd Taurox (Heavy Stubber)";
+        crew = "TIOW_Cad_Tnk667th";
+        typicalCargo[] = {"TIOW_Cad_Tnk667th"};
+		hiddenSelections[] = {"camo1"};
+		hiddenSelectionsTextures[] = {"\reskins\data\land_vehicles\tiow_taurox\143rd_AC_Grey.paa"};
+    };
+	class TIOW_Taurox_AC_667;
+	class 143rd_Taurox_Grey_Base_AC: TIOW_Taurox_HS_667
+    {
+        _generalMacro = "TIOW_Taurox_HS_667";
+        scope = 2;
+        side = 1;
+        faction = "B_143rd_DKoK";
+        displayName = "143rd Taurox (Autocannon)";
+        crew = "TIOW_Cad_Tnk667th";
+        typicalCargo[] = {"TIOW_Cad_Tnk667th"};
+		hiddenSelections[] = {"camo1"};
+		hiddenSelectionsTextures[] = {"\reskins\data\land_vehicles\tiow_taurox\143rd_AC_Grey.paa"};
+    };
 };
