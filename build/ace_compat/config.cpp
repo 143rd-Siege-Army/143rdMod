8b4f9f0e7a8ae4de5ead2a1ea9bc74138bac530f
Eric <esigde13@gmail.com>
2020-04-16 14:57:16 -0500

    fix req. addons fix.

--- build/ace_compat/config.cpp
+++ build/ace_compat/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches
 		author = "Siggyfreed";
 		name = "DK143 TIOW ACE Compat";
 		requiredAddons[] = 
-        {"ace_common", "tiow"};
+        {"ace_common", "DkoKFoot","CadFoot","OrkBoyz","TIOWSpaceMarines"};
 		requiredVersion = 0.1;
 		units[] = {};
 		weapons[] = {};
