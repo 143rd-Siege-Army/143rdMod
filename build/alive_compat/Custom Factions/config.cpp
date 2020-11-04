da54e6485d3ca91c0ae5343de7c3ae24ad25c651
Venture143 <63887505+Venture143@users.noreply.github.com>
2020-11-04 08:59:57 -0700

    Update config.cpp

--- build/alive_compat/Custom Factions/config.cpp
+++ build/alive_compat/Custom Factions/config.cpp
@@ -1,1 +1,15 @@
 
+class CfgPatches
+{
+	class DK143_ALIVE_COMPAT_Custom_Factions
+	{
+		author = "Siggyfreed";
+		name = "DK143 TIOW ALIVE_COMPAT";
+		requiredAddons[] = 
+        {"ace_common", "DkoKFoot","CadFoot","OrkBoyz","TIOWSpaceMarines"};
+		requiredVersion = 0.1;
+		units[] = {};
+		weapons[] = {};
+		worlds[] = {};
+	};
+};
