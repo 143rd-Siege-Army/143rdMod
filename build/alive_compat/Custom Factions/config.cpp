14614f4abd8be843413add7e21fa95967cbf9a30
Venture143 <63887505+Venture143@users.noreply.github.com>
2020-11-04 18:31:40 -0700

    Update config.cpp

--- build/alive_compat/Custom Factions/config.cpp
+++ build/alive_compat/Custom Factions/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches
 		author = "Venture";
 		name = "DK143 TIOW ALIVE_COMPAT Custom Factions";
 		requiredAddons[] = 
-        {"ace_common", "DkoKFoot","CadFoot","OrkBoyz","TIOWSpaceMarines"};
+        {"ace_common", "DkoKFoot","CadFoot","OrkBoyz","TIOWSpaceMarines", "AM_weapons"};
 		requiredVersion = 0.1;
 		units[] = {};
 		weapons[] = {};
@@ -30,14 +30,21 @@ class CfgFactionClasses {
 		icon = "\A3\ui_f\data\map\markers\flags\CSAT_ca.paa";
 		priority = 0;
 	};
+	
+		class DK143_O_Admech {
+		displayname = "Adeptus Mechanicus";
+		side = 0;
+		flag = "\A3\ui_f\data\map\markers\flags\CSAT_ca.paa";
+		icon = "\A3\ui_f\data\map\markers\flags\CSAT_ca.paa";
+		priority = 0;
+	};
 };
 
 class CfgGroups {
 	class EAST {
 		class DK143_Custom_Factions{
 			name = "143rd Custom Factions";
-			class DK143_O_NurgleCultists
-			{
+			class DK143_O_NurgleCultists {
 				name = "Nurgle Cultists";
 				class DK143_O_NurgleCultists_fireteam {
 					name = "Fireteam";
@@ -211,7 +218,7 @@ class CfgGroups {
 				};
 			};
 		
-			class DK143_O_Farsight_Dominate{
+			class DK143_O_Farsight_Dominate {
 				name = "Farsight Dominate";
 				class DK143_O_Farsight_Dominate_fireteam {
 					name = "Fireteam";
@@ -385,7 +392,280 @@ class CfgGroups {
 				};
 			};
 		
-		
+			class DK143_O_Admech {
+				name = "Adeptus Mechanicus";
+
+                class DK143_O_Admechfireteam {
+                    name = "Skitarii Ranger Fireteam";
+                    side = 0;
+                    faction = "DK143_O_Admech";
+                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+                    rarityGroup = 0.5;
+
+                    class Unit0 {
+                        position[] = {0,0,0};
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger_Alpha";
+                    };
+                    class Unit1 {
+                        position[] = {5,-5,0};
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit2 {
+                        position[] = {-5,-5,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit3 {
+                        position[] = {10,-10,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                };
+
+                class DK143_O_Admechfireteam_ATRilfe {
+                    name = "AT Rifle Fireteam";
+                    side = 0;
+                    faction = "DK143_O_Admech";
+                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+                    rarityGroup = 0.5;
+
+                    class Unit0 {
+                        position[] = {0,0,0};
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger_Alpha";
+                    };
+                    class Unit1 {
+                        position[] = {5,-5,0};
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger_AT_Rifle";
+                    };
+                    class Unit2 {
+                        position[] = {-5,-5,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit3 {
+                        position[] = {10,-10,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                };
+
+                class DK143_O_Admechfireteam_CQB {
+                    name = "Skitarii Vanguard Fireteam";
+                    side = 0;
+                    faction = "DK143_O_Admech";
+                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+                    rarityGroup = 0.5;
+
+                    class Unit0 {
+                        position[] = {0,0,0};
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard_Alpha";
+                    };
+                    class Unit1 {
+                        position[] = {5,-5,0};
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit2 {
+                        position[] = {-5,-5,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit3 {
+                        position[] = {10,-10,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                };
+
+
+                class DK143_O_Admechfireteam_plasma {
+                    name = "Skitarii Vanguard Fireteam Plasma";
+                    side = 0;
+                    faction = "DK143_O_Admech";
+                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+                    rarityGroup = 0.5;
+
+                    class Unit0 {
+                        position[] = {0,0,0};
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard_Alpha2";
+                    };
+                    class Unit1 {
+                        position[] = {5,-5,0};
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard_Plasma";
+                    };
+                    class Unit2 {
+                        position[] = {-5,-5,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit3 {
+                        position[] = {10,-10,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                };
+
+                class DK143_O_Admechsquad {
+                    name = "Skitarii Ranger Squad";
+                    side = 0;
+                    faction = "DK143_O_Admech";
+                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+                    rarityGroup = 0.3;
+
+                    class Unit0 {
+                        position[] = {0,0,0};
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger_Alpha";
+                    };
+                    class Unit1 {
+                        position[] = {5,-5,0};
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit2 {
+                        position[] = {-5,-5,0};
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger_AT_Rifle";
+                    };
+                    class Unit3 {
+                        position[] = {10,-10,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit4 {
+                        position[] = {-10,-10,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit5 {
+                        position[] = {15,-15,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit6 {
+                        position[] = {-15,-15,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit7 {
+                        position[] = {20,-20,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit8 {
+                        position[] = {-20,-20,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger";
+                    };
+                    class Unit9 {
+                        position[] = {25,-25,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Ranger_AT_Rifle";
+                    };
+                };
+
+				class DK143_O_Admechsquad_Vanguard {
+                    name = "Skitarii Vanguard Squad";
+                    side = 0;
+                    faction = "DK143_O_Admech";
+                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+                    rarityGroup = 0.3;
+
+                    class Unit0 {
+                        position[] = {0,0,0};
+                        rank = "SERGEANT";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard_Alpha";
+                    };
+                    class Unit1 {
+                        position[] = {5,-5,0};
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard_Alpha2";
+                    };
+                    class Unit2 {
+                        position[] = {-5,-5,0};
+                        rank = "CORPORAL";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard_Plasma";
+                    };
+                    class Unit3 {
+                        position[] = {10,-10,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit4 {
+                        position[] = {-10,-10,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit5 {
+                        position[] = {15,-15,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit6 {
+                        position[] = {-15,-15,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit7 {
+                        position[] = {20,-20,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit8 {
+                        position[] = {-20,-20,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard";
+                    };
+                    class Unit9 {
+                        position[] = {25,-25,0};
+                        rank = "PRIVATE";
+                        side = 0;
+                        vehicle = "DK143_O_AdmechSkitarii_Vanguard_Plasma";
+                    };
+                };
+			};
 		};
 	};
 	class WEST{};
@@ -411,6 +691,7 @@ class CfgVehicles
 	class O_Soldier_base_F;
 	
 	// Created Base Classes, used for inheritance only
+	// Nurgle
 	class DK143_Base_Nurgle_Cultist : O_Soldier_base_F {
 		scope = 0; // 0 = inheritance use only, not available by macro or editor, 0 = inheritance only, 1 = Macro, 2 = Editor
 		cost = 1; // AI target weight - 1 = Grunt, 4 =Squad Leader, 8 = Medic, 10 = Officer
@@ -425,7 +706,7 @@ class CfgVehicles
 		respawnlinkedItems[] = {"TIOW_Cultist_Gear","TIOW_Cultist_Hood_brown","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
 		backpack = "Ren_Backpack_02_black";
 	};
-	
+	// Farsight Dominate
 	class DK143_Base_Farsight_Dominate : O_Soldier_base_F {
 		scope = 0;
 		scopeCurator = 0;
@@ -442,7 +723,36 @@ class CfgVehicles
 
 		backpack = "TIOW_Tau_Bck_Strike_FE";
 	};
-	
+	//	Adeptus Mechanicus
+	class DK143_O_AdmechSkitarii_BaseRanger : O_Soldier_base_F {
+        scope = 0;
+        scopeCurator = 0;
+		cost = 1;
+        displayName = "Skitarii Ranger";
+        side = 0;
+        faction = "DK143_O_Admech";
+        identityTypes[] = {"Head_NATO","LanguageENG_F","NoGlasses"};
+        uniformClass = "AM_robesUNI";
+		hiddenSelections[] = {"camo"};
+        linkedItems[] = {"AM_armor","AM_hood","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+        respawnlinkedItems[] = {"AM_armor","AM_hood","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+        backpack = "BP";
+};
+
+	class DK143_O_AdmechSkitarii_BaseVanguard : O_Soldier_base_F {
+        scope = 0;
+        scopeCurator = 0;
+		cost = 1;
+        displayName = "Skitarii Vanguard";
+        side = 0;
+        faction = "DK143_O_Admech";
+        identityTypes[] = {"Head_NATO","LanguageENG_F","NoGlasses"};
+        uniformClass = "AM_alpha_robesUNI";
+		hiddenSelections[] = {"camo"};
+        linkedItems[] = {"AM_armor","AM_VH1","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+        respawnlinkedItems[] = {"AM_armor","AM_VH1","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+        backpack = "BP";
+};
 	// Fleshed out Units
 	// Nurgle Cultists
 	// OPFOR
@@ -572,5 +882,89 @@ class CfgVehicles
 
         	magazines[] = {mag_5(TIOW_railrifle_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine),"Laserbatteries","Laserbatteries"};
         	respawnMagazines[] = {mag_5(TIOW_railrifle_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine),"Laserbatteries","Laserbatteries"};
+	};
+
+	// Adeptus Mechanicus
+	class DK143_O_AdmechSkitarii_Ranger : DK143_O_AdmechSkitarii_BaseRanger {
+		scope = 2;
+        	scopeCurator = 2;
+	    	weapons[] = {"galvanic_rifle"};
+		respawnWeapons[] = {"galvanic_rifle"};
+
+		magazines[] = {mag_5(galvanic_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		respawnMagazines[] = {mag_5(galvanic_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};	
+	};
+	
+	class DK143_O_AdmechSkitarii_Vanguard : DK143_O_AdmechSkitarii_BaseVanguard {
+		scope = 2;
+		scopeCurator = 2;
+		weapons[] = {"radium_carbine"};
+        	respawnWeapons[] = {"radium_carbine"};
+
+		magazines[] = {mag_5(radium_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		respawnMagazines[] = {mag_5(radium_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+	};
+	
+	class DK143_O_AdmechSkitarii_Ranger_AT_Rifle : DK143_O_AdmechSkitarii_BaseRanger {
+		scope = 2;
+        	scopeCurator = 2;
+		cost = 3;
+		weapons[] = {"transuranic_arquebus"};
+		respawnWeapons[] = {"transuranic_arquebus"};
+		displayName = "Skitarii Transuranic Rifleman";
+		magazines[] = {mag_5(transuranic_AP_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		respawnMagazines[] = {mag_5(transuranic_AP_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		
+	};
+	
+	class DK143_O_AdmechSkitarii_Ranger_Alpha : DK143_O_AdmechSkitarii_BaseRanger	{
+		scope = 2;
+        	scopeCurator = 2;
+		linkedItems[] = {"AM_armor","AM_alphahood","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+        	respawnlinkedItems[] = {"AM_armor","AM_alphahood","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+		weapons[] = {"galvanic_rifle"};
+        	respawnWeapons[] = {"galvanic_rifle"};
+		displayName = "Skitarii Ranger Alpha";
+		magazines[] = {mag_5(galvanic_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		respawnMagazines[] = {mag_5(galvanic_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+	};
+	
+	class DK143_O_AdmechSkitarii_Vanguard_Plasma : DK143_O_AdmechSkitarii_BaseVanguard {
+		scope = 2;
+		scopeCurator = 2;
+		weapons[] = {"plasma_caliver"};
+		respawnWeapons[] = {"plasma_caliver"};
+		displayName = "Skitarii Vanguard Plasma";
+		magazines[] = {mag_5(plasma_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		respawnMagazines[] = {mag_5(plasma_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		
+	};
+
+	class DK143_O_AdmechSkitarii_Vanguard_Alpha : DK143_O_AdmechSkitarii_BaseVanguard {
+		scope = 2;
+		scopeCurator = 2;
+		uniformClass = "AM_alpha_robesUNI";
+		displayName = "Skitarii Vanguard Alpha";
+		linkedItems[] = {"AM_armor","AM_VH2","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+		respawnlinkedItems[] = {"AM_armor","AM_VH2","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+		weapons[] = {"radium_carbine"};
+       	 	respawnWeapons[] = {"radium_carbine"};
+
+		magazines[] = {mag_5(radium_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		respawnMagazines[] = {mag_5(radium_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+	};
+	
+	class DK143_O_AdmechSkitarii_Vanguard_Alpha2 : DK143_O_AdmechSkitarii_BaseVanguard {
+		scope = 2;
+        	scopeCurator = 2;
+		uniformClass = "AM_alpha_robesUNI";
+		displayName = "Skitarii Vanguard Alpha-2";
+		linkedItems[] = {"AM_armor","AM_VH3","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+		respawnlinkedItems[] = {"AM_armor","AM_VH3","ItemMap","ItemRadio","ItemCompass","ItemWatch"};
+		weapons[] = {"radium_carbine"};
+        	respawnWeapons[] = {"radium_carbine"};
+
+		magazines[] = {mag_5(radium_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
+		respawnMagazines[] = {mag_5(radium_mag), mag_2(TIOW_ig_frag_grenade_mag), mag_2(ML700_krak_grenade_magazine)};
 	};
 };
